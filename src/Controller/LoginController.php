de5713916aecc0352a4d795f1e436e983e4a8506
marko.stevic <marko.stevic@zeastim.com>
2022-07-11 01:44:39 +0200

    Login and register with roles

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function index(ManagerRegistry $doctrine, AuthenticationUtils $authenticationUtils): Response
+    {
+        $user = $doctrine->getRepository(User::class)->find(4);
+
+        dd($user->getRoles());
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
